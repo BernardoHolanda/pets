7f6cbc854ad74e29ce887179c33dfcfbcdfb8b20
Holanda <bernardoholada@gmail.com>
2026-04-22 11:21:30 -0400

    feat: Implementing pet_lister_controller

---
+++ src/controllers/pet_lister_controller.py
@@ -0,0 +1,28 @@
+from src.models.sqlite.interfaces.pets_repository_interface import PetsRepositoryInterface
+from src.models.sqlite.entities.pets import PetsTable
+
+class PetListerController():
+    def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
+        self.__pet_repository = pet_repository
+
+    def list(self) -> dict:
+        pets = self.__get_pets_in_db()
+        formated_response = self.__format_response(pets)
+        return formated_response
+
+    def __get_pets_in_db(self) -> list[PetsTable]:
+        pets = self.__pet_repository.list_pets()
+        return pets
+
+    def __format_response(self, pets: list[PetsTable]) -> dict:
+        formated_pets = []
+        for pet in pets:
+            formated_pets.append({ "name": pet.name, "id": pet.id  })
+
+        return {
+            "data": {
+                "type": "Pets",
+                "count": len(formated_pets),
+                "attributes": formated_pets
+            }
+        }
